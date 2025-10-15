ea3e6c005ae80b33b2590683e2804876da4e94b6
RebecaZaia <rebeca.zaia@estudante.ifms.edu.br>
2025-10-15 02:22:29 -0300

    acho que faltou isso

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 import express from "express"
 const app = express()
+app.use(express.json())
+
+const usuarios = []
 
 app.get('/', (req, res) => {
     res.send("Você está na página principal")
 })
 
-app.post('/', (req, res) => {
-    res.send("Você está na página principal usando post")
+app.get('/usuario', (req, res) => {
+    res.status(200).json(usuarios)
+})
+
+app.post('/usuario', (req, res) => {
+    const {nome, idade} = req.body
+    usuarios.push({nome, idade})
+    console.log(usuarios)
+    res.status(201).send("Usuário Criado")
 })
 
 app.listen(3333, () => {
