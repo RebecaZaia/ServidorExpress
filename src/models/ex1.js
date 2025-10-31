a750eb88d6fee4040c51401382af26899be24f42
Rebeca Zaia <rebeca.zaia@estudante.ifms.edu.br>
2025-10-31 14:14:23 -0400

    Delete src/models/ex1.js

--- src/models/ex1.js
+++
@@ -1,8 +0,0 @@
-import {Schema, model} from "mongoose";
-
-const exemploPrato = new Schema({
-  nome: {type: String, requied:true},
-  preco: {type: Number}
-})
-
-const PratoModel = model('Prato', prato)
